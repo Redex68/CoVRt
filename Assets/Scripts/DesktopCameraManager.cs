271a70d4432778931990e3c6f0b147641073122e
David Kaméus <david.kameus@gmail.com>
2023-09-08 16:44:01 +0200

    Implement camera switching

---
+++ Assets/Scripts/DesktopCameraManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DesktopCameraManager : MonoBehaviour
+{
+
+    // inputSystem embedded actions
+    [SerializeField]
+    private InputAction nextCamera;
+    [SerializeField]
+    private InputAction prevCamera;
+    
+    
+    
+    private Camera mainCamera;
+
+    [SerializeField]
+    private Transform[] cameraPoses;
+
+    [SerializeField]
+    private int currentCamera = 0;
+    void Awake() {
+        // get a reference to the camera
+        mainCamera = GetComponentInChildren<Camera>();
+        // bind callbacks for input system
+        nextCamera.performed += _ => {StepCamera(1);};
+        prevCamera.performed += _ => {StepCamera(-1);};
+    }
+
+    /*
+    StepCamera - changes the current camera position to the previous or next position.
+    @param step - 1 to move to the next camera, -1 to go to the previous camera.
+    */
+    void StepCamera(int step) {
+        currentCamera = (currentCamera + step) % cameraPoses.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform targetPose = cameraPoses[currentCamera];
+        mainCamera.transform.SetPositionAndRotation(targetPose.position, targetPose.rotation);
+    }
+
+    void OnEnable() {
+        nextCamera.Enable();
+        prevCamera.Enable();
+    }
+    void OnDisable() {
+        nextCamera.Disable();
+        prevCamera.Disable();
+    }
+}
