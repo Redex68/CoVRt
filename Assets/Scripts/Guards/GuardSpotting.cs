2c1a2b1a63ed3f8c71a64e5c17e4ea1bdc9b915f
Redex <marin.petric73@gmail.com>
2023-09-09 17:31:18 +0200

    Fixed game over not reseting

--- Assets/Scripts/Guards/GuardSpotting.cs
+++ Assets/Scripts/Guards/GuardSpotting.cs
@@ -11,8 +11,6 @@ public class GuardSpotting : MonoBehaviour
     FloatVariable guardFOV;
     [SerializeField]
     GameEvent playerSpotted;
-    [SerializeField]
-    BoolVariable gameOver;
 
     // Start is called before the first frame update
     void Start()
@@ -23,12 +21,25 @@ public class GuardSpotting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Show raycast
+        //if (Physics.Raycast(transform.position, player.transform.position - transform.position, out RaycastHit hitInfoTest) && hitInfoTest.transform.gameObject.name == "Player")
+        //{
+        //    if(PlayerIsInFront())
+        //        Debug.DrawLine(transform.position, hitInfoTest.point, Color.green);
+        //    else
+        //        Debug.DrawLine(transform.position, hitInfoTest.point, Color.blue);
+        //}
+        //else
+        //    Debug.DrawLine(transform.position, hitInfoTest.point, Color.red);
+
+
         //Show FOV in Scene editor
         //Debug.DrawLine(transform.position, Vector3.RotateTowards(transform.forward, transform.right, guardFOV.Value / 2.0f / 180f * Mathf.PI, 0) * 100 + transform.position);
         //Debug.DrawLine(transform.position, Vector3.RotateTowards(transform.forward, -transform.right,guardFOV.Value / 2.0f / 180f * Mathf.PI, 0) * 100 + transform.position);
-        if(player != null && !gameOver.Value)
+        
+        if(player != null && !State.gameOver)
         {
-            if(Vector3.Angle(transform.forward, player.transform.position - transform.position) < guardFOV.Value / 2.0f)
+            if(PlayerIsInFront())
             {
                 //TODO: Make a better check for whether the ray hit the player (tag?)
                 //TODO: Add layermask
@@ -36,9 +47,13 @@ public class GuardSpotting : MonoBehaviour
                 {
                     Debug.Log("In LOS");
                     playerSpotted.Raise(this, null);
-                    gameOver.Value = true;
+                    State.gameOver = true;
                 }
             }
         }
+    }
+
+    private bool PlayerIsInFront() {
+        return Vector3.Angle(transform.forward, player.transform.position - transform.position) < guardFOV.Value / 2.0f;
     }
 }
