47b9b48d89dab7bfbee61bcd27e371458497af9e
Tobias Mattsson <tmatts@kth.se>
2023-09-11 19:06:14 +0200

    Incorporate overview cam feed to desktop interface

---
+++ Assets/Scripts/OverviewMap.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverviewMap : MonoBehaviour
+{
+    private int layer;
+    public Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        layer = 6;
+    }
+
+    public void SwitchFloor(int floor)
+    {
+        if (floor >= 1 && floor <= 5) layer = floor + 5;
+        else layer = 6;
+        cam.cullingMask = (1 << layer) | (1 << LayerMask.NameToLayer("Stairs"));
+    }
+}
