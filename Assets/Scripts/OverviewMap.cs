dd459cefa8084dfbfd62ccf623276d28d7148fb4
Tobias Mattsson <tmatts@kth.se>
2023-09-14 12:03:01 +0200

    Use ortographic camera for overview map

--- Assets/Scripts/OverviewMap.cs
+++ Assets/Scripts/OverviewMap.cs
@@ -15,8 +15,21 @@ public class OverviewMap : MonoBehaviour
 
     public void SwitchFloor(int floor)
     {
-        if (floor >= 1 && floor <= 5) layer = floor + 5;
-        else layer = 6;
-        cam.cullingMask = (1 << layer) | (1 << LayerMask.NameToLayer("Stairs")) | (1 << LayerMask.NameToLayer("MapIcon"));
+        //if (floor >= 1 && floor <= 5) layer = floor + 5;
+        //else layer = 6;
+        //cam.cullingMask = (1 << layer) | (1 << LayerMask.NameToLayer("Stairs")) | (1 << LayerMask.NameToLayer("MapIcon"));
+
+        // for floor 1
+        if (floor == 1)
+        {
+            cam.nearClipPlane = 95;
+            cam.farClipPlane = 105;
+        }
+        // for floor 2
+        else if (floor == 2)
+        {
+            cam.nearClipPlane = 85;
+            cam.farClipPlane = 95;
+        }
     }
 }
