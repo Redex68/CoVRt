c054e897cc2858b8f0801ef9c9c43da526b6eee3
druskus20 <druskus20@gmail.com>
2023-09-16 19:02:31 +0200

    setup

--- Assets/Scripts/Guards/GuardNavigation.cs
+++ Assets/Scripts/Guards/GuardNavigation.cs
@@ -13,14 +13,14 @@ public class GuardNavigation : MonoBehaviour
     /// <summary> A list of waypoints for the guard's patroll path. Will randomly choose a point to walk towards. </summary>
     [SerializeField]
     List<Transform> waypoints = new List<Transform>();
-    
+
     private NavMeshAgent agent;
     private bool waiting = false;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        if(waypoints.Count == 0) Debug.LogError("Guard has no waypoints");
+        if (waypoints.Count == 0) Debug.LogError("Guard has no waypoints");
     }
 
     // Update is called once per frame
@@ -29,17 +29,18 @@ public class GuardNavigation : MonoBehaviour
         if (!waiting && !agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
         {
             if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
-            {            
-                if(waypoints.Count != 0)
+            {
+                if (waypoints.Count != 0)
                     StartCoroutine(pickNewWaypoint());
             }
         }
     }
 
-    private IEnumerator pickNewWaypoint() {
+    private IEnumerator pickNewWaypoint()
+    {
         waiting = true;
         yield return new WaitForSeconds(delay);
-        
+
         int indx = UnityEngine.Random.Range(0, waypoints.Count);
         agent.SetDestination(waypoints[indx].position);
         Debug.Log("\"" + name + "\" going to \"" + waypoints[indx].name + "\"");
