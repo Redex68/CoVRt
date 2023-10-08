0e3f441ac96e48d194f20c22ea5636ca62fa85fe
Tobias Mattsson <tmatts@kth.se>
2023-10-08 22:36:27 +0200

    Added simple sound occlusion

--- Assets/Scripts/SoundController.cs
+++ Assets/Scripts/SoundController.cs
@@ -6,6 +6,8 @@ using FMOD.Studio;
 public class SoundController : MonoBehaviour
 {
     public StudioEventEmitter emitter;
+
+    [Space]
     [Range(0, 500)]
     [Tooltip("Volume multiplier (%)")]
     public int volume = 100; 
@@ -15,6 +17,25 @@ public class SoundController : MonoBehaviour
     [Range(0, 100)]
     [Tooltip("Reverb multiplier (%)")]
     public int reverb = 0;
+
+    [Space]
+    public bool useOcclusion = false;
+    [Range(0, 10)]
+    public float occlusionSpread = 1;
+    public LayerMask occlusionLayers;
+
+    // occlusion stuff
+    private StudioListener listener;
+    private int lineCastsObstructed;
+    private int totalLineCasts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // find the audio listener in the scene
+        listener = FindObjectOfType<StudioListener>();
+        totalLineCasts = 3;
+    }
 
     // Update is called once per frame
     void Update()
@@ -27,5 +48,73 @@ public class SoundController : MonoBehaviour
         emitter.EventInstance.setReverbLevel(1, (float)reverb / 100);
         emitter.EventInstance.setReverbLevel(2, (float)reverb / 100);
         emitter.EventInstance.setReverbLevel(3, (float)reverb / 100);
+
+        if (useOcclusion) Occlusion();
+    }
+
+    [ContextMenu("Play")]
+    private void Play()
+    {
+        emitter.EventInstance.getPaused(out bool paused);
+        if (paused) Pause();
+        else emitter.Play();
+    }
+
+    [ContextMenu("Pause")]
+    private void Pause()
+    {
+        emitter.EventInstance.getPaused(out bool paused);
+        emitter.EventInstance.setPaused(!paused);
+    }
+
+    private void Occlusion()
+    {
+        // NOTE: only occlude if sound can be heard (is close enough)
+        // check out isVirtual property of the event instance
+
+        lineCastsObstructed = 0;
+
+        OcclusionLineCast(listener.transform.position, transform.position);
+        OcclusionLineCast(CalculatePoint(listener.transform.position, transform.position, occlusionSpread, true), transform.position);
+        OcclusionLineCast(CalculatePoint(listener.transform.position, transform.position, occlusionSpread, false), transform.position);
+
+        emitter.SetParameter("Occlusion", (float)lineCastsObstructed / totalLineCasts);
+        emitter.EventInstance.setParameterByName("Occlusion", (float)lineCastsObstructed / totalLineCasts);
+    }
+
+    // Not mine; this is by ScottGamesSounds https://scottgamesounds.com/wp-content/uploads/2020/01/C.FirstPersonOcclusion.txt
+    private Vector3 CalculatePoint(Vector3 a, Vector3 b, float m, bool posOrneg)
+    {
+        float x;
+        float z;
+        float n = Vector3.Distance(new Vector3(a.x, 0f, a.z), new Vector3(b.x, 0f, b.z));
+        float mn = (m / n);
+        if (posOrneg)
+        {
+            x = a.x + (mn * (a.z - b.z));
+            z = a.z - (mn * (a.x - b.x));
+        }
+        else
+        {
+            x = a.x - (mn * (a.z - b.z));
+            z = a.z + (mn * (a.x - b.x));
+        }
+        return new Vector3(x, a.y, z);
+    }
+
+    private void OcclusionLineCast(Vector3 listener, Vector3 sound)
+    {
+        RaycastHit hit;
+        Physics.Linecast(listener, sound, out hit, occlusionLayers);
+
+        Color color;
+        if (hit.collider)
+        {
+            lineCastsObstructed++;
+            color = Color.red;
+        }
+        else color = Color.green;
+
+        Debug.DrawLine(listener, sound, color);
     }
 }
